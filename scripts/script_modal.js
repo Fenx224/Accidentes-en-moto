9511db3553360ff8e783d6645a58c54bc380a497
Fenx <zagj080422hgtvrsa8@cbtis217.edu.mx>
2025-12-15 08:37:28 -0600

    v0.1.11 Finalizacion de los estilos para las ventanas modales, eliminacion de archivos inecesarios y re organizacion del codigo CSS

--- scripts/script_modal.js
+++ scripts/script_modal.js
@@ -1,5 +1,5 @@
 const modal = document.querySelector(".modal");
-const abrir = document.getElementById("btnAgregar");
+const abrir = document.querySelector(".btnAgregar");
 const cerrar = document.getElementById("cerrar");
 
 if (abrir) {
@@ -10,5 +10,21 @@ if (abrir) {
 if (cerrar) {
     cerrar.onclick = function() {
         modal.style.display = "none";
+    }
+}
+
+/* Script para el modal de modificar */
+const modalUpdate = document.querySelector(".modalUpdate");
+const abrirModificar = document.querySelector(".btnUpdate");
+const cerrarModificar = document.getElementById("closeModalUpdate");
+
+if (abrirModificar) {
+    abrirModificar.onclick = function() {
+        modalUpdate.style.display = "block";
+    }
+}
+if (cerrarModificar) {
+    cerrarModificar.onclick = function() {
+        modalUpdate.style.display = "none";
     }
 }
